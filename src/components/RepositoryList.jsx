71c2cebe9813fae7780fce37b2a8c1b6afd59071
EngJao89 <eng_joao@outlook.com>
2023-07-09 00:00:52 -0300

    feat(ge02-componentes): implementa primeiro componente do sistema

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+    <h1>Lista de repositórios</h1>
+
+    <ul>
+      <li>
+        <p>Technologies</p>
+      </li>
+      <strong>ReactJS</strong>
+      <strong>React Native</strong>
+      <strong>NodeJS</strong>
+    </ul>
+  </section>
+  )
+}
